5e928ff2ad487024ab3fd709497a9a48bd878161
Amadeus Alfa <alfa@torwell.de>
2016-03-27 18:47:50 +0200

    + v1

--- lib/bird.sh
+++ lib/bird.sh
@@ -23,7 +23,7 @@ bird_init() {
 	
 	echo -n "" > conf/bird-routes.local.conf
 	for a in "${SERVICE_ADDRESSES[@]}"; do
-		if [ $(bird_check_route "$a") = 1 ] && bird_add_route "$a"
+		[ $(bird_check_route "$a") = 1 ] && bird_add_route "$a"
 	done
 }
 
