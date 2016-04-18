da3fe790e736f123dadb1f2203e8166a11c8ffbf
vsandre <vsandre@web.de>
2016-04-18 12:29:41 +0200

    Update the status image only every 30 min.

--- lib/meshviewer.sh
+++ lib/meshviewer.sh
@@ -9,7 +9,14 @@ meshviewer_init() {
 
 # Called by watchdog
 meshviewer_cron() {
-	/opt/freifunk/meshviewer/ffmap-backend/backend.py -d /opt/freifunk/meshviewer/data/ --rrd-path /opt/freifunk/meshviewer/data/nodedb --with-rrd --with-img
+        local cronTime=$(date +%s)
+
+        if [ $(($cronTime%1800)) -lt 10 ]; then
+                # every 30 min with image creation
+                /opt/freifunk/meshviewer/ffmap-backend/backend.py -d /opt/freifunk/meshviewer/data/ --rrd-path /opt/freifunk/meshviewer/data/nodedb --with-rrd --with-img
+        else
+                /opt/freifunk/meshviewer/ffmap-backend/backend.py -d /opt/freifunk/meshviewer/data/ --rrd-path /opt/freifunk/meshviewer/data/nodedb --with-rrd
+        fi
 	jq -c '.nodes = (.nodes | map(del(.value.nodeinfo.owner)))' < /opt/freifunk/meshviewer/data/nodes.json > /opt/freifunk/meshviewer/data/nodes.json.priv
 }
 
