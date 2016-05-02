5f1fd66f136ebded5c12ce427a7ce3372ce03282
Steffen <ambassador86@users.noreply.github.com>
2016-05-02 15:50:02 +0200

    insert fastd backbone config
    + advance fastd backbone config to a dependency
    + add start script for backbone

--- lib/fastd.sh
+++ lib/fastd.sh
@@ -4,9 +4,15 @@ fastd_init() {
 	if [ ! -r "conf/fastd-secret.local.conf" ]; then
 		log_fatal_error "Missing fastd-secret.local.conf - please check configuration!"
 	fi
+	
+	if [ ! -r "conf/fastd.backbone.local.conf" ]; then
+		log_fatal_error "Missing fastd.backbone.local.conf - please check configuration!"
+	fi
 }
 
 fastd_start() {
+	fastd -d -c conf/fastd.backbone.local.conf
+	
 	fastd -d -c conf/fastd.conf
 }
 
