782dc683d5a2732f8f2bc0e3e9503c46a8ab9e5c
Steffen Förster <ambassador86@users.noreply.github.com>
2016-05-16 08:38:49 +0200

    added init banner
    this ensures the start after all important services

--- initd-ffc.sh
+++ initd-ffc.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+### BEGIN INIT INFO
+# Provides:		ffc
+# Required-Start:	$all
+# Required-Stop:	
+# Default-Start:	2 3 4 5
+# Default-Stop:		0 1 6
+# Short-Description:	Freifunk Server Scripts
+### END INIT INFO
+
+
 cd /opt/freifunk/server-scripts
 ./ffc-server.sh "$1"
 
