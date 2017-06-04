b85a39a025f99ab0f4629b7e78799576893748d2
Sven Eckelmann <sven@narfation.org>
2017-06-04 07:30:28 +0200

    Generate KML files from nodelist

--- lib/meshviewer.sh
+++ lib/meshviewer.sh
@@ -28,6 +28,7 @@ meshviewer_cron() {
 	OFFLINE_THRESHOLD=60 /opt/freifunk/meshviewer/ffv-nodes2eventlog/nodes2eventlog.py /var/www/meshviewer/ffv/nodes.json /opt/freifunk/meshviewer/ffv-nodes2eventlog/db-threshold60 /var/www/meshviewer/ffv/eventlog-threshold60.atom
 
 	/opt/freifunk/ffv-api-generator/api-gen.py /var/www/meshviewer/ffv/nodelist.json /var/www/meshviewer/ffv/
+	/opt/freifunk/meshviewer/nodelist2kml/nodelist2kml.py /var/www/meshviewer/ffv/nodelist.json /var/www/meshviewer/ffv/nodelist.kml
 
 	/opt/freifunk/ffv-grafana-config/generate-dashboards.py  /var/www/meshviewer/ffv/nodelist.json /opt/freifunk/ffv-grafana-config/dashboard-templates/ /opt/freifunk/ffv-grafana-config/dashboard/dynamic/
 }
