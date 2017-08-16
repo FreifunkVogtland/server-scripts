ce779ce66e8f487ddc710641855ce9d2cd00fbe2
Sven Eckelmann <sven@narfation.org>
2017-08-16 17:37:45 +0200

    Generate readable list of offline nodes

--- lib/meshviewer.sh
+++ lib/meshviewer.sh
@@ -15,6 +15,8 @@ meshviewer_cron() {
 
 	/opt/freifunk/meshviewer/ffv-nodes2eventlog/nodes2eventlog.py /var/www/meshviewer/ffv/nodes.json /opt/freifunk/meshviewer/ffv-nodes2eventlog/db /var/www/meshviewer/ffv/eventlog.atom
 	OFFLINE_THRESHOLD=60 /opt/freifunk/meshviewer/ffv-nodes2eventlog/nodes2eventlog.py /var/www/meshviewer/ffv/nodes.json /opt/freifunk/meshviewer/ffv-nodes2eventlog/db-threshold60 /var/www/meshviewer/ffv/eventlog-threshold60.atom
+	/opt/freifunk/meshviewer/ffv-nodes2eventlog/graveyard2rst.py /opt/freifunk/meshviewer/ffv-nodes2eventlog/db-threshold60 /var/www/meshviewer/ffv/graveyard.rst
+	pandoc -f rst -t html5 -o /var/www/meshviewer/ffv/graveyard.html /var/www/meshviewer/ffv/graveyard.rst
 
 	/opt/freifunk/ffv-api-generator/api-gen.py /var/www/meshviewer/ffv/nodelist.json /var/www/meshviewer/ffv/
 	/opt/freifunk/meshviewer/nodelist2kml/nodelist2kml.py /var/www/meshviewer/ffv/nodelist.json /var/www/meshviewer/ffv/nodelist.kml
