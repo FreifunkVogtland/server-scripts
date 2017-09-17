1d03703e01047c27016b46c25642341d2d6167c1
Sven Eckelmann <sven@narfation.org>
2017-09-17 17:47:49 +0200

    Remove blackhole default route for IPv4
    Bird is actually responsible to handle it - and it does this by marking
    routes as unreachable instead of just sending traffic towards an invalid
    route.

--- lib/bird.sh
+++ lib/bird.sh
@@ -6,7 +6,6 @@ bird_init() {
 	ip rule add from 185.66.195.42/31 lookup 100
 	ip rule add from 185.66.194.70/31 lookup 100
 	ip rule add from all fwmark 0x1 table 100
-	ip route add default via 127.0.0.1 table 100 metric 1024
 	
 	iptables -w -t nat -A POSTROUTING -o $WANIF -j MASQUERADE
 	iptables -w -t mangle -A FORWARD -o bb-+ -p tcp -m tcp --tcp-flags SYN,RST SYN -m tcpmss ! --mss 0:1240 -j TCPMSS --set-mss 1240
